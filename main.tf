016e4787d02540147f6a74ec1afc2edd44ed770b
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-22 21:19:01 +0530

    dfg

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_docdb_cluster" "docdbr" {
   cluster_identifier      = "${var.env}-docdb"
   engine                  = var.engine
   engine_version          = var.engine_version
-  master_username         = data.aws_ssm_parameter.docdb-user
-  master_password         = data.aws_ssm_parameter.docdb-pass
+  master_username         = data.aws_ssm_parameter.docdb-user.value
+  master_password         = data.aws_ssm_parameter.docdb-pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = var.skip_final_snapshot
