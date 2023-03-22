302b96ac11d25fedd9db3af3076f7dab014f7edd
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-22 21:05:26 +0530

    dfg

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
 resource "aws_docdb_cluster" "docdbr" {
   cluster_identifier      = "${var.env}-docdb"
   engine                  = var.engine
+  engine_version          = var.engine_version
   master_username         = data.aws_ssm_parameter.docdb-user
   master_password         = data.aws_ssm_parameter.docdb-pass
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = var.skip_final_snapshot
+  db_subnet_group_name    = aws.aws_docdb_subnet_group.subnet_id
 }
 
 resource "aws_docdb_subnet_group" "subgrpr" {
   name       = "${var.env}-docdb-subnetgroup"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+  subnet_ids = var.subnet_ids
 
-  tags = {
-    Name = "My docdb subnet group"
-  }
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-subnet-group" }
+  )
 }
---
+++ vars.tf
@@ -0,0 +1,22 @@
+variable "env" {
+  
+}
+variable "engine" {
+  
+}
+variable "backup_retention_period" {
+  
+}
+variable "preferred_backup_window" {
+  
+}
+variable "skip_final_snapshot" {
+  
+}
+variable "subnet_ids" {
+  
+}
+
+
+
+
